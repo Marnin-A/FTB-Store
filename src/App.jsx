7262aa17767bcb5bd8ffb7db3218260bde531080
Marnin Audu <msmaudu@gmail.com>
2023-03-11 21:20:26 +0100

    Finish products component

--- src/App.jsx
+++ src/App.jsx
@@ -15,6 +15,7 @@ function App() {
       <Navbar />
       <Hero />
       <Featured />
+      <Products />
     </div>
   );
 }
--- src/components/products/products.jsx
+++ src/components/products/products.jsx
@@ -1,5 +1,42 @@
 import React from "react";
-
+import img1 from "../../assets/Rectangle14.png";
+import img2 from "../../assets/Rectangle15.png";
+import img3 from "../../assets/Rectangle16.png";
+import img4 from "../../assets/Rectangle17.png";
+import arrow from "../../assets/right-arrow.svg";
+import styles from "./products.module.css";
 export const Products = () => {
-  return <div>products</div>;
+  const Images = [
+    { img: img1, name: "FTB 14 Armoured BLEND" },
+    { img: img2, name: "FTB 14 BLUEhorse BLEND" },
+    { img: img3, name: "FTB 14 Lightsaber BLEND" },
+    { img: img4, name: "FTB 14 REDBULL express" },
+  ];
+  return (
+    <section className={styles.products}>
+      <div className={styles.products_header}>
+        <div>OUR Gamepad BLENDS</div>
+        <div>
+          View Product <img id={styles.arrow} src={arrow} alt="" />
+        </div>
+      </div>
+      <div className={styles.items}>
+        {Images.map((element) => {
+          return <Card img={element.img} name={element.name} />;
+        })}
+      </div>
+    </section>
+  );
 };
+
+function Card({ img, name }) {
+  return (
+    <div className={styles.card}>
+      <img id={styles.controller} src={img} alt="A playstation controller" />
+      <span>{name}</span>
+      <span>
+        More Info <img id={styles.arrow} src={arrow} alt="" />
+      </span>
+    </div>
+  );
+}

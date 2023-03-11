b6afe3459c01b08bed3492eaffe22ab936faf4a7
Marnin Audu <msmaudu@gmail.com>
2023-03-11 22:46:06 +0100

    Finish story component

--- src/App.jsx
+++ src/App.jsx
@@ -16,6 +16,7 @@ function App() {
       <Hero />
       <Featured />
       <Products />
+      <Story />
     </div>
   );
 }
--- src/components/story/story.jsx
+++ src/components/story/story.jsx
@@ -1,5 +1,25 @@
 import React from "react";
+import styles from "./story.module.css";
+import background from "../../assets/2nd-bckgrnd.png";
 
 export const Story = () => {
-  return <div>story</div>;
+  return (
+    <div className={styles.story}>
+      <div className={styles.text}>
+        <div>HELPING GAMERS MOVE FORWARD</div>
+        <div>
+          Ftb 14 Gamepad Company's beginnings are founded in the history of the
+          2nd/14th Light Horse Regiment (Queensland Mounted Infantry). We are a
+          proud Badagrian small business, serving local communities and giving
+          back to support Badagrian committed gamers and their families.
+        </div>
+        <button>Read our Story</button>
+      </div>
+      <img
+        id={styles.bg_img}
+        src={background}
+        alt="A game room with call of duty playing in the background"
+      />
+    </div>
+  );
 };
